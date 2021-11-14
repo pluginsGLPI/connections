68977f4130a047fe43bfaf945fcb91e478098e1d
tsmr <thetsmr@gmail.com>
2021-11-14 17:08:36 +0100

    Fix utf8mb4_unicode_ci

--- sql/empty-10.0.sql
+++ sql/empty-10.0.sql
@@ -3,18 +3,18 @@ CREATE TABLE `glpi_plugin_connections_connections` (
    `id` int(11) NOT NULL auto_increment,
    `entities_id` int(11) NOT NULL default '0',
    `is_recursive` tinyint(1) NOT NULL default '0',
-   `name` varchar(255) collate utf8_unicode_ci default NULL,
+   `name` varchar(255) collate utf8mb4_unicode_ci default NULL,
    `plugin_connections_connectiontypes_id` int(11) NOT NULL default '0' COMMENT 'RELATION to glpi_plugin_connections_connectiontypes (id)',
    `plugin_connections_connectionrates_id` int(11) NOT NULL default '0' COMMENT 'RELATION to glpi_plugin_connections_connectionrates (id)',
    `plugin_connections_guaranteedconnectionrates_id` int(11) NOT NULL default '0' COMMENT 'RELATION to glpi_plugin_connections_connectionratesguaranteed (id)',
-   `bytes` varchar(255) collate utf8_unicode_ci NOT NULL default '',
+   `bytes` varchar(255) collate utf8mb4_unicode_ci NOT NULL default '',
    `users_id_tech` int(11) NOT NULL default '0' COMMENT 'RELATION to glpi_users (id)',
    `groups_id_tech` int(11) NOT NULL default '0' COMMENT 'RELATION to glpi_groups (id)',
    `suppliers_id` int(11) NOT NULL default '0' COMMENT 'RELATION to glpi_suppliers (id)',
    `locations_id` int(11)NOT NULL DEFAULT '0' COMMENT 'RELATION to glpi_locations (id)',
-   `comment` text collate utf8_unicode_ci,
-   `notepad` longtext collate utf8_unicode_ci,
-   `others` varchar(255) collate utf8_unicode_ci default NULL,
+   `comment` text collate utf8mb4_unicode_ci,
+   `notepad` longtext collate utf8mb4_unicode_ci,
+   `others` varchar(255) collate utf8mb4_unicode_ci default NULL,
    `is_helpdesk_visible` int(11) NOT NULL default '1',
    `date_mod` timestamp NULL DEFAULT NULL,
    `is_deleted` tinyint(1) NOT NULL default '0',
@@ -37,7 +37,7 @@ CREATE TABLE `glpi_plugin_connections_connections_items` (
    `id` int(11) NOT NULL auto_increment,
    `plugin_connections_connections_id` int(11) NOT NULL default '0',
    `items_id` int(11) NOT NULL default '0' COMMENT 'RELATION to various tables, according to itemtype (id)',
-   `itemtype` varchar(100) collate utf8_unicode_ci NOT NULL COMMENT 'see .class.php file',
+   `itemtype` varchar(100) collate utf8mb4_unicode_ci NOT NULL COMMENT 'see .class.php file',
    PRIMARY KEY  (`id`),
    UNIQUE KEY `unicity` (`plugin_connections_connections_id`,`itemtype`,`items_id`),
    KEY `FK_device` (`items_id`,`itemtype`),
@@ -48,8 +48,8 @@ DROP TABLE IF EXISTS `glpi_plugin_connections_connectiontypes`;
 CREATE TABLE `glpi_plugin_connections_connectiontypes` (
    `id` int(11) NOT NULL auto_increment,
    `entities_id` int(11) NOT NULL default '0',
-   `name` varchar(255) collate utf8_unicode_ci default NULL,
-   `comment` text collate utf8_unicode_ci,
+   `name` varchar(255) collate utf8mb4_unicode_ci default NULL,
+   `comment` text collate utf8mb4_unicode_ci,
    `is_recursive` tinyint(1) NOT NULL default '0',
    PRIMARY KEY  (`id`),
    KEY `name` (`name`)
@@ -59,8 +59,8 @@ DROP TABLE IF EXISTS `glpi_plugin_connections_connectionrates`;
 CREATE TABLE `glpi_plugin_connections_connectionrates` (
    `id` int(11) NOT NULL auto_increment,
    `entities_id` int(11) NOT NULL default '0',
-   `name` varchar(255) collate utf8_unicode_ci default NULL,
-   `comment` text collate utf8_unicode_ci,
+   `name` varchar(255) collate utf8mb4_unicode_ci default NULL,
+   `comment` text collate utf8mb4_unicode_ci,
    `is_recursive` tinyint(1) NOT NULL default '0',
    PRIMARY KEY  (`id`),
    KEY `name` (`name`)
@@ -70,8 +70,8 @@ DROP TABLE IF EXISTS `glpi_plugin_connections_guaranteedconnectionrates`;
 CREATE TABLE `glpi_plugin_connections_guaranteedconnectionrates` (
    `id` int(11) NOT NULL auto_increment,
    `entities_id` int(11) NOT NULL default '0',
-   `name` varchar(255) collate utf8_unicode_ci default NULL,
-   `comment` text collate utf8_unicode_ci,
+   `name` varchar(255) collate utf8mb4_unicode_ci default NULL,
+   `comment` text collate utf8mb4_unicode_ci,
    `is_recursive` tinyint(1) NOT NULL default '0',
    PRIMARY KEY  (`id`),
    KEY `name` (`name`)
@@ -80,8 +80,8 @@ CREATE TABLE `glpi_plugin_connections_guaranteedconnectionrates` (
 DROP TABLE IF EXISTS `glpi_plugin_connections_configs`;
 CREATE TABLE `glpi_plugin_connections_configs` (
    `id` int(11) NOT NULL auto_increment,
-   `delay_expired` varchar(50) collate utf8_unicode_ci NOT NULL default '30',
-   `delay_whichexpire` varchar(50) collate utf8_unicode_ci NOT NULL default '30',
+   `delay_expired` varchar(50) collate utf8mb4_unicode_ci NOT NULL default '30',
+   `delay_whichexpire` varchar(50) collate utf8mb4_unicode_ci NOT NULL default '30',
    PRIMARY KEY  (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ROW_FORMAT=DYNAMIC;
 
