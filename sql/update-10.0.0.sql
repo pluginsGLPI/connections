b0185695a5b354a85973b2aeb396e2f1be57d23d
tsmr <thetsmr@gmail.com>
2022-01-29 10:15:24 +0100

    Fix update 10

--- sql/update-10.0.0.sql
+++ sql/update-10.0.0.sql
@@ -1,5 +1,5 @@
 ALTER TABLE `glpi_plugin_connections_connections`
-CHANGE `users_id` `users_id_tech` int(11) NOT NULL default '0' COMMENT 'RELATION to glpi_users (id)';
+CHANGE `users_id` `users_id_tech` int unsigned NOT NULL default '0' COMMENT 'RELATION to glpi_users (id)';
 ALTER TABLE `glpi_plugin_connections_connections`
-    CHANGE `groups_id` `groups_id_tech` int(11) NOT NULL default '0' COMMENT 'RELATION to glpi_groups (id)';
+    CHANGE `groups_id` `groups_id_tech` int unsigned NOT NULL default '0' COMMENT 'RELATION to glpi_groups (id)';
 
